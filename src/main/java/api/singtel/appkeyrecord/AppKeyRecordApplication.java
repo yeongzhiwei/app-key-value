d03199f3a5d794a2a511ad60b930c6a6b5f58f27
zhiwei yeong <zhiwei.yeong@singtel.com>
2020-08-04 17:11:35 +0800

    add starter files

---
+++ src/main/java/api/singtel/appkeyrecord/AppKeyRecordApplication.java
@@ -0,0 +1,13 @@
+package api.singtel.appkeyrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppKeyRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppKeyRecordApplication.class, args);
+	}
+
+}
