2e116a8d0ecef8fa773b0fad88878fe436442ad0
Zhi Wei Yeong <zhiwei.yeong@singtel.com>
2020-08-13 12:41:55 +0800

    move securityconfig to its own folder

--- src/main/java/api/singtel/appkeyrecord/security/SecurityConfig.java
+++ src/main/java/api/singtel/appkeyrecord/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package api.singtel.appkeyrecord;
+package api.singtel.appkeyrecord.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
